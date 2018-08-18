8c43283e9d5f3da0df1b12105ea3dc81f04cc555
sykes836 <bernard.sykes836@gmail.com>
2018-08-18 12:11:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
+})
